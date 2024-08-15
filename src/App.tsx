f67c5260356b57cb9fdfd7918d6b7580a36e5c16
Alexander Smith <hongzeyuan0@gmail.com>
2024-08-15 17:37:40 +0800

    useState---自动推导

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [str, setStr] = useState("121");
+  const [bool, setBool] = useState(true);
+  const [obj, setObj] = useState({ name: "jack", age: 18, address: "beijing" });
+  const [arr, setArr] = useState();
+
+  const handleChange = () => {
+    setStr("1");
+    setBool(false);
+    setObj({ name: "jack", age: 18, address: "beijing" });
+    setArr(1);
+  };
   return (
     <>
       <div>123</div>
