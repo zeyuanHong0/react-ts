2b986b32ff82ddb148098d83d0bbfe32e9d0231c
Alexander Smith <hongzeyuan0@gmail.com>
2024-08-16 20:16:16 +0800

    useState---泛型参数

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  const [str, setStr] = useState("121");
-  const [bool, setBool] = useState(true);
-  const [obj, setObj] = useState({ name: "jack", age: 18, address: "beijing" });
-  const [arr, setArr] = useState();
+  // const [str, setStr] = useState("121");
+  // const [bool, setBool] = useState(true);
+  // const [obj, setObj] = useState({ name: "jack", age: 18, address: "beijing" });
+  // const [arr, setArr] = useState();
+
+  // const handleChange = () => {
+  //   setStr("1");
+  //   setBool(false);
+  //   setObj({ name: "jack", age: 18, address: "beijing" });
+  //   // setArr(1);
+  // };
+  type User = {
+    name: string;
+    age: number;
+  };
+  const [user, setUser] = useState<User>();
 
   const handleChange = () => {
-    setStr("1");
-    setBool(false);
-    setObj({ name: "jack", age: 18, address: "beijing" });
-    setArr(1);
+    setUser({ name: "jack", age: 18 });
   };
   return (
     <>
